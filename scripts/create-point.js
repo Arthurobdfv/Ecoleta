cd86fd208bd26f3e1015bc7e4ec293a7da5c512a
Arthurobdfv <38329059+Arthurobdfv@users.noreply.github.com>
2020-06-08 07:20:27 +0200

    Day 2

---
+++ scripts/create-point.js
@@ -0,0 +1,62 @@
+
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]");
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( (res) => { return res.json() })
+    .then( states => {
+        for(state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+        }
+    } )
+}
+
+populateUFs();
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]");
+    const stateInput = document.querySelector("input[name=state]");
+    const ufSelected = event.target.value;
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+    citySelect.disabled = true;
+
+    const indexOfSelectedState = event.target.selectedIndex;
+    stateInput.value = event.target.options[indexOfSelectedState].text;
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufSelected}/municipios`)
+    .then( (res) => { return res.json() })
+    .then( cities => {
+        for(city of cities){
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
+        }
+
+        citySelect.disabled = false;
+    } );
+
+}
+
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for(let item of itemsToCollect){
+    item.addEventListener("click", handleSelectItem)
+}
+
+let selectedItems = [];
+
+
+function handleSelectItem (event) {
+    
+    const itemLi = event.target;
+    console.log("clicked")
+    
+    itemLi.classList.toggle("selected");
+    
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex( item => item === itemId);
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId;
+        })
+    }
+}
